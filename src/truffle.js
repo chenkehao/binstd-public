1643fac13b947b7a13111f5b56bffbf3102eee3b
wanggl <42193152@qq.com>
2018-05-18 00:44:47 +0800

    add truffle frame

---
+++ src/truffle.js
@@ -0,0 +1,49 @@
+module.exports = {
+  build: {
+    "index.html": "index.html",
+    "app.js": [
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/app.css"
+    ],
+    "images/": "images/"
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
+  },
+  networks: {
+ "live": {
+    network_id: 1, // Ethereum public network
+    // optional config values
+    // host - defaults to "localhost"
+    // port - defaults to 8545
+    // gas
+    // gasPrice
+    // from - default address to use for any transaction Truffle makes during migrations
+  },
+  "morden": {
+    network_id: 2,        // Official Ethereum test network
+    host: "178.25.19.88", // Random IP for example purposes (do not use)
+    port: 80
+  },
+  "staging": {
+    network_id: 112223, // custom private network
+     // custom private network
+    host:"localhost",
+    port: 9545,
+    gas:"0x2fefd8"
+	// use default rpc settings
+  },
+  "development": {
+    network_id: "default"
+  }
+	//"development": {
+	 // network_id: "112223", // custom private network
+	 // host:"localhost",
+	 // port: 8545,
+	 // gas:"0x2fefd8"
+	//},
+  }
+};
